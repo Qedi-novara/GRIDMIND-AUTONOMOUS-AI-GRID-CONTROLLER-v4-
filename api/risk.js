eb562e6519560791ce75d48b886288d89ca904ae
Qedi-novara <tinashesithole35@gmail.com>
2026-04-24 01:51:39 +0200

    Update risk.js

--- api/risk.js
+++ api/risk.js
@@ -1,13 +1,3 @@
-module.exports = (req, res) => {
-
-  const load = 680;
-  const risk = {
-    level: "LOW"
-  };
-
-  if (load > 600) risk.level = "MEDIUM";
-  if (load > 650) risk.level = "HIGH";
-  if (load > 700) risk.level = "CRITICAL";
-
-  res.status(200).json(risk);
-};
+export default function handler(req, res) {
+  res.json({ risk: "LOW" });
+}
