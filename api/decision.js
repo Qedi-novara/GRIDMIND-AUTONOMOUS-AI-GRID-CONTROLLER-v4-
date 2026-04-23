04a8da1cc123d73c1113987d4de28e958fe07622
Qedi-novara <tinashesithole35@gmail.com>
2026-04-24 01:49:24 +0200

    Update decision.js

--- api/decision.js
+++ api/decision.js
@@ -1,17 +1,3 @@
-module.exports = (req, res) => {
-
-  const load = 600;
-  const supply = 550;
-
-  let action = "STABLE";
-
-  if (load > supply) {
-    action = "REDUCE LOAD / SHIFT POWER";
-  }
-
-  if (load > 650) {
-    action = "EMERGENCY MODE";
-  }
-
-  res.status(200).json({ action });
-};
+export default function handler(req, res) {
+  res.json({ action: "STABLE" });
+}
