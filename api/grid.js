9f54ee68ed317dc6819de4310ae703f215684c4e
Qedi-novara <tinashesithole35@gmail.com>
2026-04-24 01:48:06 +0200

    Update grid.js

--- api/grid.js
+++ api/grid.js
@@ -1,10 +1,3 @@
-module.exports = (req, res) => {
-
-  const data = {
-    harare: 500 + Math.random() * 50,
-    bulawayo: 300 + Math.random() * 40,
-    mutare: 180 + Math.random() * 20
-  };
-
-  res.status(200).json(data);
-};
+export default function handler(req, res) {
+  res.json({ harare: 500, bulawayo: 300 });
+}
