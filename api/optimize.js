27ab5bfdd1f35f32432568445808240c8c1cc8bc
Qedi-novara <tinashesithole35@gmail.com>
2026-04-24 01:50:32 +0200

    Update optimize.js

--- api/optimize.js
+++ api/optimize.js
@@ -1,20 +1,3 @@
-module.exports = (req, res) => {
-
-  const grid = {
-    harare: 520,
-    bulawayo: 280
-  };
-
-  let transfer = 0;
-
-  if (grid.harare > 500) {
-    transfer = 20;
-    grid.harare -= transfer;
-    grid.bulawayo += transfer;
-  }
-
-  res.status(200).json({
-    message: "Power redistributed",
-    grid
-  });
-};
+export default function handler(req, res) {
+  res.json({ harare: 480, bulawayo: 320 });
+}
